6827068388ad90880608e849073b9b2024f93ff5
JaeRyoungKa <jaeryoung.ka@kaist.ac.kr>
2016-07-21 00:50:47 +0900

    balance changes

--- app/src/main/java/com/example/q/neobjukgo/StuffCreature.java
+++ app/src/main/java/com/example/q/neobjukgo/StuffCreature.java
@@ -30,10 +30,10 @@ public class StuffCreature extends Stuff {
 
     }
     public void attack(StuffCreature target) {
-        double HPLost = (target.getAttack()/1.5 - this.getArmor()/2);
-        double DMGDealt = (this.getAttack()/1.5 - target.getArmor()/2);
+        double HPLost = (int) (target.getAttack()/5 -this.getArmor()/2);
+        double DMGDealt = (int) (1+(this.getAttack()/2 - (target.getArmor()/2)));
         if (HPLost < 0 ) HPLost = 0;
-        if (DMGDealt < 0) DMGDealt = 0;
+        if (DMGDealt <= 0) DMGDealt = 0.5;
         this.health -= HPLost;
         target.health -= DMGDealt;
         ManagerLogger.getInstance().log(target.toString()+"(으)로부터 "+HPLost+"의 피해를 입었습니다.");
