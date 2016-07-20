69f3bc5a9b6fe5eb00ccbd8b01d9be8e02c320bd
JaeRyoungKa <jaeryoung.ka@kaist.ac.kr>
2016-07-20 23:36:23 +0900

    minor patch

--- app/src/main/java/com/example/q/neobjukgo/StuffItem.java
+++ app/src/main/java/com/example/q/neobjukgo/StuffItem.java
@@ -9,7 +9,6 @@ public class StuffItem extends  Stuff {
 
     public int item_performance;
     Random random = new Random();
-    int category = random.nextInt(2);
 
     public StuffItem(int item_performance) {
         this.item_performance = item_performance;
